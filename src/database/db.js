7c94073aca1d4d45a3f938fd8c751a583c2a510f
Guilherme Preveda Milék <guilhermeprevedamilek@gmail.com>
2022-07-07 16:35:25 -0300

    Create initial folder structure

---
+++ src/database/db.js
@@ -0,0 +1,12 @@
+// src > database > db.js
+/*
+
+Aqui ficará um banco de dados (database) falso para sua aplicação,
+ou seja, simular uma local que guardará dados útei para seu projeto.
+
+Você pode salvar dados que precisam ser acessados por
+diversas partes do projeto, como por exemplo, um carrinho de compras:
+
+const shoppingCart = [];
+
+*/
